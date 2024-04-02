09a8545fff9256cc9758d50cd4265829b8c0c214
cxzadm <cx.practica@gmail.com>
2024-04-01 23:24:51 -0500

    first commit

---
+++ app/src/main/java/com/bryam/appchatkotlin/SplashScreen.kt
@@ -0,0 +1,28 @@
+package com.bryam.appchatkotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        mostrarBienvenida()
+    }
+
+    private fun mostrarBienvenida() {
+        object : CountDownTimer(3000, 1000) {
+            override fun onTick(p0: Long) {
+                // En caso de que necesites hacer algo en cada tick del temporizador
+            }
+
+            override fun onFinish() {
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.start()
+    }
+}
