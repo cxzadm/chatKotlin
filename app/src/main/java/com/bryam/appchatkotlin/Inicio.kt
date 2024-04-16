5629685f832539047e059f03c8e0fae31104cfb3
cxzadm <cx.practica@gmail.com>
2024-04-16 00:51:42 -0500

    pantalla_inicio_verificar_seccion_activa

--- app/src/main/java/com/bryam/appchatkotlin/Inicio.kt
+++ app/src/main/java/com/bryam/appchatkotlin/Inicio.kt
@@ -6,11 +6,16 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class Inicio : AppCompatActivity() {
 
     private lateinit var  Btn_ir_registros : Button
     private lateinit var  Btn_ir_logeo : Button
+
+    var firebaseUser : FirebaseUser?=null
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,5 +35,18 @@ class Inicio : AppCompatActivity() {
             Toast.makeText(applicationContext, "Login", Toast.LENGTH_SHORT).show()
             startActivity(intent)
         }
+    }
+    private fun ComprobarSesion(){
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+        if (firebaseUser!=null){
+            val intent = Intent(this@Inicio, MainActivity::class.java)
+            Toast.makeText(applicationContext, "La sesión está activa", Toast.LENGTH_SHORT).show()
+            startActivity(intent)
+            finish()
+        }
+    }
+    override fun onStart() {
+        ComprobarSesion()
+        super.onStart()
     }
 }
