9ad4789fe0c0bd7c6058761f45b620753cb34fd9
cxzadm <cx.practica@gmail.com>
2024-04-18 00:50:26 -0500

    inicio_secion_google_mejora_interfaz

--- app/src/main/java/com/bryam/appchatkotlin/Inicio.kt
+++ app/src/main/java/com/bryam/appchatkotlin/Inicio.kt
@@ -1,41 +1,148 @@
 package com.bryam.appchatkotlin
 
+import android.app.ProgressDialog
 import android.content.Intent
-import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.button.MaterialButton
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.database.FirebaseDatabase
 
 class Inicio : AppCompatActivity() {
 
-    private lateinit var  Btn_ir_registros : Button
-    private lateinit var  Btn_ir_logeo : Button
+    private lateinit var Btn_ir_logeo : MaterialButton
+    private lateinit var Btn_login_google : MaterialButton
 
     var firebaseUser : FirebaseUser?=null
     private lateinit var auth : FirebaseAuth
 
+    private lateinit var progressDialog : ProgressDialog
+    private lateinit var mGoogleSignInClient : GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_inicio)
 
-        Btn_ir_registros = findViewById(R.id.Btn_ir_registros)
         Btn_ir_logeo = findViewById(R.id.Btn_ir_logeo)
+        Btn_login_google = findViewById(R.id.Btn_login_google)
+        auth = FirebaseAuth.getInstance()
 
-        Btn_ir_registros.setOnClickListener {
-            val intent = Intent(this@Inicio, RegistroActivity::class.java)
-            Toast.makeText(applicationContext, "Registros", Toast.LENGTH_SHORT).show()
-            startActivity(intent)
-        }
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Espere por favor")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
         Btn_ir_logeo.setOnClickListener {
             val intent = Intent(this@Inicio, LoginActivity::class.java)
-            Toast.makeText(applicationContext, "Login", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(applicationContext, "Login", Toast.LENGTH_SHORT).show()
             startActivity(intent)
         }
+
+        Btn_login_google.setOnClickListener {
+            EmpezarinicioSesionGoogle()
+        }
     }
+
+    private fun EmpezarinicioSesionGoogle() {
+        val googleSignIntent = mGoogleSignInClient.signInIntent
+        googleSignInARL.launch(googleSignIntent)
+    }
+
+    private val googleSignInARL = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) {resultado->
+        if (resultado.resultCode == RESULT_OK){
+            val data = resultado.data
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                AutenticarGoogleFirebase(account.idToken)
+            }catch (e: Exception){
+                Toast.makeText(applicationContext, "Ha ocurrido una excepción debido a ${e.message}", Toast.LENGTH_SHORT).show()
+
+            }
+        }else{
+            Toast.makeText(applicationContext, "Cancelado", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun AutenticarGoogleFirebase(idToken: String?) {
+        val credencial = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credencial)
+            .addOnSuccessListener { authResult->
+                /*Si el usuario es nuevo*/
+                if (authResult.additionalUserInfo!!.isNewUser){
+                    GuardarInfoBD()
+                }
+                /*Si el usuario ya se registró previamente*/
+                else{
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finishAffinity()
+                }
+
+            }.addOnFailureListener { e->
+                Toast.makeText(applicationContext, "${e.message}", Toast.LENGTH_SHORT).show()
+
+            }
+
+    }
+
+    private fun GuardarInfoBD() {
+        progressDialog.setMessage("Se está registrando su información...")
+        progressDialog.show()
+
+        /*Obtener información de una cuenta de Google*/
+        val uidGoogle = auth.uid
+        val correoGoogle = auth.currentUser?.email
+        val n_Google = auth.currentUser?.displayName
+        val nombre_usuario_G : String = n_Google.toString()
+
+        val hashmap = HashMap<String, Any?>()
+        hashmap["uid"] = uidGoogle
+        hashmap["n_usuario"] = nombre_usuario_G
+        hashmap["email"] = correoGoogle
+        hashmap["imagen"] = ""
+        hashmap["buscar"] = nombre_usuario_G.lowercase()
+
+        /*Nuevos datos de usuario*/
+        hashmap["nombres"] = ""
+        hashmap["apellidos"] =""
+        hashmap["edad"] = ""
+        hashmap["profesion"] = ""
+        hashmap["domicilio"] = ""
+        hashmap["telefono"] = ""
+        hashmap["estado"] = "offline"
+        hashmap["proveedor"] = "Google"
+
+        /*Referencia a la base de datos*/
+        val reference = FirebaseDatabase.getInstance().getReference("Usuarios")
+        reference.child(uidGoogle!!)
+            .setValue(hashmap)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+                Toast.makeText(applicationContext, "Se ha registrado exitosamente", Toast.LENGTH_SHORT).show()
+                finishAffinity()
+            }
+            .addOnFailureListener { e->
+                progressDialog.dismiss()
+                Toast.makeText(applicationContext, "${e.message}", Toast.LENGTH_SHORT).show()
+            }
+
+    }
+
     private fun ComprobarSesion(){
         firebaseUser = FirebaseAuth.getInstance().currentUser
         if (firebaseUser!=null){
@@ -45,6 +152,7 @@ class Inicio : AppCompatActivity() {
             finish()
         }
     }
+
     override fun onStart() {
         ComprobarSesion()
         super.onStart()
